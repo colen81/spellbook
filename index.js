60b0ec9d32e9ed2273bc8014a5a6d92147353045
Steve Norton <colen81@gmail.com>
2018-06-05 15:39:32 -0400

    Please

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ const changeHeading = function(ev) {
   const f = ev.target
   const spellName = f.spellName.value
 
-  document
-    .querySelector('#spells')
-<<<<<<< HEAD
-    .innerHTML += '<p>' + spellName + '</p>'
-    f.reset()
-    
-=======
-    .textContent += ' ' + spellName
-    f.reset()
->>>>>>> 7e24ba87f9311087275d44c87ca8e0821821aa2f
+  const spellsDiv = document.querySelector('#spells')
+  spellsDiv.innerHTML += `<p>${spellName}</p>`
+
+  f.reset()
 }
 
 form.addEventListener('submit', changeHeading)
