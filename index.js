70b0a0e400a5eb8bd0b746cdc7853b695a586fe9
Steve Norton <colen81@gmail.com>
2018-06-05 15:08:35 -0400

    reset the form after submission

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const button = document.querySelector('button')
-const heading = document.getElementById('1h')
-const secondHeading = document.getElementById('two')
-const subButton = document.getElementById('example')
-// inital variables
-function sayYeah()
-{
-  heading.textContent = "JavaScript is fun"
-  // change heading to that input
-}
-button.addEventListener('click',sayYeah,)
-// add event listener ot button
-function sayNo()
-{
-  var formInput = document.getElementById('input2').value
-  // the text input value is assinged to formInput
-  secondHeading.textContent = formInput
-  // text content of the second heading is assigned to formINput
-}
-subButton.addEventListener('click',sayNo,)
-// add event listener to submit button
+const form = document.querySelector('form')
 
-document.addEventListener('keyup',function(event)
-{
-  event.preventDefault()
-  if (event.keyCode === 13)
-  {
-    var formInput2 = document.getElementById('input2').value
-    secondHeading.textContent = formInput2
-    return true;
-  }
-})
+const changeHeading = function(ev) {
+  ev.preventDefault()
+
+  const f = ev.target
+  const spellName = f.spellName.value
+
+  document
+    .querySelector('#spells')
+    .textContent += ' ' + spellName
+    f.reset()
+}
+
+form.addEventListener('submit', changeHeading)
