7c0270dfb9001a3afeebe091182bb1970abd846b
Steve Norton <colen81@gmail.com>
2018-06-05 09:39:50 -0400

    first two parts done

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const button = document.querySelector('button')
+const heading = document.querySelector('h1')
+const secondHeading = document.getElementById('two')
+var formInput = document.getElementById('input2').value
+const subButton = document.querySelector('sbm')
 
 function sayYeah()
 {
-  button.textContent = "CLICKED"
-  const heading = document.querySelector('h1')
-  heading.textContent = "i hate javascript"
-  //x = 9
-  // without identifier creates global variable
+  secondHeading.textContent = "JavaScript is fun"
 }
 button.addEventListener('click',sayYeah)
+subButton.addEventListener('useForm',function()
+{
+  console.log('formInput')
+  heading.textContent = formInput
+})
+
 
