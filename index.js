52f47d7744832fc5f72b765f060776e2fddc2bec
Steve Norton <colen81@gmail.com>
2018-06-06 14:03:59 -0400

    Add css, add second button

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const form = document.querySelector('form')
-const but = document.querySelector('button')
+const but = document.querySelector('button');
+var array = []
 const changeHeading = function(ev) {
   ev.preventDefault()
 
   const f = ev.target
   const spellName = f.spellName.value
-  console.log(spellName)
+  array.push(spellName)
   const spellsDiv = document.querySelector('#spells')
-  spellsDiv.innerHTML += `<p>${spellName}</p>`
+  const button = document.createElement('button')
+
+  spellsDiv.appendChild(button)
+  console.log(array)
+  console.log(spellName)
+  for (let i = 0; i< array.length;i++)
+  {
+    if (array.includes(spellName))
+  {
+      spellsDiv.innerHTML += `<p>${spellName}</p>`
+  }
+  }
   const color = f.spellNames.value
   
     if (color == 'javascript')
