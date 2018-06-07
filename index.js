3e3bb7a474bea79bc3ced0d475242edb62c9d096
Steve Norton <colen81@gmail.com>
2018-06-07 14:27:43 -0400

    Make me insane

--- index.js
+++ index.js
@@ -1,81 +1,72 @@
+var array = []
 const app = {
   init: function() {
-    this.spells = []
-
     const form = document.querySelector('form')
-    form.addEventListener('submit', (ev) => {
-      ev.preventDefault()
+    form.addEventListener('submit', ev => {
       this.handleSubmit(ev)
     })
   },
 
   renderProperty: function(name, value) {
     const el = document.createElement('span')
-    el.classList.add(name)
     el.textContent = value
-    el.setAttribute('title', value)
+    el.classList.add(name)
     return el
   },
 
   renderItem: function(spell) {
     // ['name', 'level']
-    const properties = Object.keys(spell)
+    properties = Object.keys(spell)
 
-    // collect an array of <span> elements
-    const childElements = properties.map((prop) => {
-      return this.renderProperty(prop, spell[prop])
+    // collect an array of renderProperty's return values
+    // (an array of <span> elements)
+    const childElements = properties.map(property => {
+      return this.renderProperty(property, spell[property])
     })
 
     const item = document.createElement('li')
     item.classList.add('spell')
 
     // append each <span> to the <li>
-    childElements.forEach(function(el) {
+    childElements.forEach(el => {
       item.appendChild(el)
     })
-
-    // add the delete button
-    const deleteButton = document.createElement('button')
-    deleteButton.classList.add('delete')
-    deleteButton.textContent = 'del'
-    deleteButton.addEventListener(
-      'click',
-      this.removeSpell.bind(this, spell)
-    )
-
-    item.appendChild(deleteButton)
 
     return item
   },
 
-  removeSpell: function(spell, ev) {
-    // Remove from the DOM
-    const button = ev.target
-    const item = button.closest('.spell')
-    item.parentNode.removeChild(item)
-
-    // Remove from the array
-    const i = this.spells.indexOf(spell)
-    this.spells.splice(i, 1)
-  },
-
   handleSubmit: function(ev) {
+    ev.preventDefault()
+
     const f = ev.target
 
     const spell = {
       name: f.spellName.value,
       level: f.level.value,
     }
-
-    this.spells.push(spell)
-
+    var obj = {}
+    obj["name"] = spell.name
+    obj["level"] = spell.level
+    array.push(obj)
+   
     const item = this.renderItem(spell)
 
     const list = document.querySelector('#spells')
     list.appendChild(item)
+    const delB = document.createElement('button')
+    delB.textContent = 'Delete'
+    delB.type = 'click'
+
+    item.appendChild(delB)
+    delB.addEventListener('click',function()
+    {
+      list.removeChild(item)
+      item.removeChild(delB)
+    })
 
     f.reset()
   },
-}
 
+  
+}
 app.init()
